4122db806637eb6dcc5b394ea6f1fea99cf55e22
kelvinyelyen <kelvinyelyen@gmail.com>
2024-04-30 11:09:59 +0000

    [update] user-dropdown color

--- components/layout/user-dropdown.tsx
+++ components/layout/user-dropdown.tsx
@@ -26,14 +26,14 @@ export default function UserDropdown({ session }: { session: Session }) {
               <p className="text-sm">Dashboard</p>
             </Link> */}
             <button
-              className="relative flex w-full cursor-not-allowed items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
+              className="relative flex w-full cursor-not-allowed items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-muted"
               disabled
             >
               <LayoutDashboard className="h-4 w-4" />
               <p className="text-sm">Dashboard</p>
             </button>
             <button
-              className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
+              className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-muted"
               onClick={() => signOut()}
             >
               <LogOut className="h-4 w-4" />
